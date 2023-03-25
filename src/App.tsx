2cb7c75b22d4316419f4ac7c1a75cf462bdb0ade
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2023-03-25 17:08:38 +0200

    Components imported to App

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Layout from './components/Layout';
+import Gallery from './components/Gallery';
+
+function App() {
+  return (
+    <Layout>
+      <Gallery />
+    </Layout>
+  );
+}
+
+export default App;
